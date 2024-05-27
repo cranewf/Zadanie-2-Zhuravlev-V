30eb8a9509cfb0a1055c0266b4d3b6122338eb58
Vadim Zhuravlev <vadim.zhuravlev120498@mail.ru>
2024-05-27 18:52:46 +0300

    update variables

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,18 @@
 public class Main {
     public static void main(String[] args) {
 
-        int amount = 50; // У клиента на счету 50 руб
-        int popolnenie = 999; // Сумма пополнения
-        int casback = 100; // 1 бонусный рубль за каждые 100 руб
+        int accountBalance = 50; // У клиента на счету 50 руб
+        int creditAmount = 950; // Сумма пополнения
+        int cashback = 100; // 1 бонусный рубль за каждые 100 руб
         int bonus; // Бонус за пополнение
 
-        if (popolnenie > 1000) {
-            bonus = popolnenie/casback;
+        if (creditAmount > 1000) {
+            bonus = creditAmount / cashback;
         } else {
             bonus = 0;
         }
 
-        int balance = amount + popolnenie + bonus;
+        int balance = accountBalance + creditAmount + bonus;
 
         System.out.println("Ваш баланс " + balance + " рублей");
         System.out.println("Ваши бонусы " + bonus + " рублей");
